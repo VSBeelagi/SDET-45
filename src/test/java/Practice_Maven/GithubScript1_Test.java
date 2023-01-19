20393e361cc1fc2c9f1cfba0d6b0a54f3aa3e0f6
vijayalaxmibeelagi10@gmail.com <vijayalaxmibeelagi10@gmail.com>
2023-01-19 10:48:28 +0530

    updated

---
+++ src/test/java/Practice_Maven/GithubScript1_Test.java
@@ -0,0 +1,14 @@
+package Practice_Maven;
+
+import org.testng.annotations.Test;
+
+public class GithubScript1_Test {
+	
+	@Test
+	
+	public void Script1test()
+	{
+		System.out.println("--- GithubScript--1 --");
+	}
+
+}
